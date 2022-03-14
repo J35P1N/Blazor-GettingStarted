283e60c21afc56c287853cd8bea237495fdc5288
Jason Espin <jason.matthew.espin@gmail.com>
2022-03-14 18:02:15 +0000

    Initial commit

---
+++ BethanysPieShopHRM.App/BethanysPieShopHRM.App/Pages/EmployeeOverview.cs
@@ -0,0 +1,19 @@
+﻿using BethanysPieShopHRM.App.Services;
+using BethanysPieShopHRM.Shared;
+using Microsoft.AspNetCore.Components;
+
+namespace BethanysPieShopHRM.App.Pages
+{
+    public partial class EmployeeOverview
+    {
+        public IEnumerable<Employee> Employees { get; set; }
+
+        [Inject]
+        public IEmployeeDataService EmployeeDataService { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Employees = (await EmployeeDataService.GetAllEmployees()).ToList();
+        }
+	}
+}
---
+++ BethanysPieShopHRM.App/BethanysPieShopHRM.App/Program.cs
@@ -0,0 +1,12 @@
+using BethanysPieShopHRM.App;
+using BethanysPieShopHRM.App.Services;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient<IEmployeeDataService, EmployeeDataService>(client => client.BaseAddress = new Uri("https://localhost:44340/"));
+
+await builder.Build().RunAsync();
